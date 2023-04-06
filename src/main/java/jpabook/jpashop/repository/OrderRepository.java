16fc2c25db9869fb407f603bda953cae812d3c9b
HYUNJUN, KIM <denny10002@naver.com>
2023-04-06 17:35:09 +0900

    JPA를 이용한 화면 구현

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -23,6 +23,14 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch){}
+//    public List<Order> findAll(OrderSearch orderSearch){
+//        em.createQuery("select o from Order o join o.member m" +
+//                    " where o.status = :status " +
+//                    " and m.name like :name", Order.class)
+//                .setParameter("status", orderSearch.getOrderStatus())
+//                .setParameter("name", orderSearch.getMemberName())
+//                .setMaxResults(1000)
+//                .getResultList();
+//    }
 
 }
